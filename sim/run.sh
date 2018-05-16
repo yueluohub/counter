37b6333414e301ef7349d9706e4ee7cf10b15b5a
Yue Luo@service <yue.luo@inplay-tech.com>
2018-05-15 23:16:23 -0700

    modify spyglass doc scripts

--- sim/run.sh
+++ sim/run.sh
@@ -1,3 +1,7 @@
+#!bin/bash
+
+test_name="test01" 
+
 ncverilog \
 +incdir+../rtl/  \
 -f counter_filelist.f  \
@@ -7,11 +11,14 @@ ncverilog \
 -top counter_top_tb_top \
 +access+rwc \
 +define+NC_SIM \
+-covfile ./covfile.ccf -covtest $test_name -covoverwrite
 #+gui 
 
 ##+define+VERI_SIM \
 ##+loadpli1=debpli:novas_pli_boot \
 ##+gui
+# -covfile ../../covfile.ccf -covtest $test_name -covoverwrite
+
 
 #+define+NC_SIM \
 #ncverilog \
@@ -19,3 +26,7 @@ ncverilog \
 #+incdir+../rtl/ -f counter_filelist.f -f int_filelist.f -nctop counter_top -l sim.log
 #irun +incdir+../rtl/  \
 ##+gui
+#  imc -gui & 
+# imc -exec merge_cov.imc //merge -run_file merge.file   -out merged4 -metrics all
+# imc -exec gen_cov_report.imc
+# 
