a660c62184603899d34e899a9986a3d5059a5f7e
yue.luo@work_laptop <yue.luo@inplay-tech.com>
2018-05-25 17:39:18 +0800

    add bfm_iso7816

---
+++ testcase/bfm_smartcard_iso7816.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps 
+module bfm_smartcard_iso7816(
+    input wire i_clk;
+    input wire i_rstn;
+    inout wire io_data
+);
+
+parameter PARA_F=32'd372;
+            PARA_D=32'd1;
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
